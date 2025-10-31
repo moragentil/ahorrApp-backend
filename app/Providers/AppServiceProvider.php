91502bd04b518dea565bde7eb89ca51a6fba4534
moragentil <moragentil@gmail.com>
2025-10-31 11:34:19 -0300

    registro de servicios y rutas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Services\Interface\IngresoServiceInterface;
-use App\Services\Implementation\IngresoService;
+
+// Interfaces
 use App\Services\Interface\GastoServiceInterface;
-use App\Services\Implementation\GastoService;
-use App\Services\Interface\UserServiceInterface;
-use App\Services\Implementation\UserService;
 use App\Services\Interface\CategoriaServiceInterface;
-use App\Services\Implementation\CategoriaService;
+use App\Services\Interface\IngresoServiceInterface;
 use App\Services\Interface\AhorroServiceInterface;
+use App\Services\Interface\GrupoGastoServiceInterface;
+use App\Services\Interface\GastoCompartidoServiceInterface;
+use App\Services\Interface\AporteGastoServiceInterface;
+
+// Implementaciones
+use App\Services\Implementation\GastoService;
+use App\Services\Implementation\CategoriaService;
+use App\Services\Implementation\IngresoService;
 use App\Services\Implementation\AhorroService;
+use App\Services\Implementation\GrupoGastoService;
+use App\Services\Implementation\GastoCompartidoService;
+use App\Services\Implementation\AporteGastoService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,11 +29,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(IngresoServiceInterface::class, IngresoService::class);
+        // Servicios existentes
         $this->app->bind(GastoServiceInterface::class, GastoService::class);
-        $this->app->bind(UserServiceInterface::class, UserService::class);
         $this->app->bind(CategoriaServiceInterface::class, CategoriaService::class);
+        $this->app->bind(IngresoServiceInterface::class, IngresoService::class);
         $this->app->bind(AhorroServiceInterface::class, AhorroService::class);
+
+        // Nuevos servicios de gastos compartidos
+        $this->app->bind(GrupoGastoServiceInterface::class, GrupoGastoService::class);
+        $this->app->bind(GastoCompartidoServiceInterface::class, GastoCompartidoService::class);
+        $this->app->bind(AporteGastoServiceInterface::class, AporteGastoService::class);
     }
 
     /**
