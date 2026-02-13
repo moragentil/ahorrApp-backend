42b61e9cce8ab066dbb6ba2382d9611c2976a9b2
moragentil <moragentil@gmail.com>
2026-02-13 10:08:46 -0300

    habilitar cors

--- config/cors.php
+++ config/cors.php
@@ -3,13 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://20.197.225.62',
-        'http://localhost:5174',
-        'http://localhost:5173',
-        'http://127.0.0.1:5174',
-        'http://127.0.0.1:5173',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
